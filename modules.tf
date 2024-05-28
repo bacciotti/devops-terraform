e5e6c0e8d22a6fd339627eb9dc1970e6c4b53f8e
Lucas Bacciotti <lbacciot@redhat.com>
2024-05-28 19:04:38 +0100

    feat: wip

--- modules.tf
+++ modules.tf
@@ -1,4 +1,4 @@
-module "eks_networks" {
+module "eks_network" {
   source       = "./modules/network"
   cidr_block   = var.cidr_block
   project_name = var.project_name
@@ -9,16 +9,16 @@ module "eks_cluster" {
   source           = "./modules/cluster"
   project_name     = var.project_name
   tags             = local.tags
-  public_subnet_1a = module.eks_networks.subnet_pub_1a
-  public_subnet_1b = module.eks_networks.subnet_pub_1b
+  public_subnet_1a = module.eks_network.subnet_pub_1a
+  public_subnet_1b = module.eks_network.subnet_pub_1b
 }
 
 module "eks_managed_node_group" {
   source            = "./modules/managed-node-group"
   project_name      = var.project_name
   cluster_name      = module.eks_cluster.cluster_name
-  subnet_private_1a = module.eks_networks.subnet_priv_1a
-  subnet_private_1b = module.eks_networks.subnet_priv_1b
+  subnet_private_1a = module.eks_network.subnet_priv_1a
+  subnet_private_1b = module.eks_network.subnet_priv_1b
   tags              = local.tags
 }
 
@@ -26,4 +26,6 @@ module "eks_aws_load_balancer_controller" {
   source       = "./modules/aws-load-balancer-controller"
   project_name = var.project_name
   tags         = local.tags
+  oidc         = module.eks_cluster.oidc
+  cluster_name = module.eks_cluster.cluster_name
 }
