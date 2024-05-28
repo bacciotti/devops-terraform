6a6250402e7667d6fe64b55d3c6655f1dffe0d36
Lucas Bacciotti <lbacciot@redhat.com>
2024-05-28 09:31:28 +0100

    feat: route tables

---
+++ igw.tf
@@ -0,0 +1,24 @@
+resource "aws_internet_gateway" "iks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = merge(
+    local.tags, {
+      Name                     = "${var.project_name}-igw",
+    }
+  )
+}
+
+resource "aws_route_table" "eks_public_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.iks_igw.id
+  }
+
+  tags = merge(
+    local.tags, {
+      Name                     = "${var.project_name}-pub-routetable",
+    }
+  )
+}
